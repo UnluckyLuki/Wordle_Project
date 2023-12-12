5a4a0bdfdb083c950729a75e2317776ed528dc48
artur-weclawski <weclawski.artur@gmail.com>
2023-12-12 17:59:22 +0100

    Tales and Buttons

---
+++ Wordle_Project/PlayAgainButton.swift
@@ -0,0 +1,27 @@
+//
+//  PlayAgainButton.swift
+//  Wordle_Project
+//
+//  Created by student on 12/12/2023.
+//
+
+import SwiftUI
+
+struct PlayAgainButton: View {
+    var isGuessed: Bool = false
+    var body: some View {
+        Button("Zagraj ponownie"){
+            
+        }.disabled(isGuessed)
+        .padding(2)
+        .frame(width: 150, height: 40)
+        .tint(.blue)
+        .border(isGuessed ? .gray : .blue, width: 2)
+        .cornerRadius(5)
+
+    }
+}
+
+#Preview {
+    PlayAgainButton()
+}
