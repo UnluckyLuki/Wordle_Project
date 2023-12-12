9641d1e5920789e1ab7a4d22f8c1c97da54d392b
Lukasz Wazelin <lukasz.wazelin@gmail.com>
2023-12-12 16:26:34 +0100

    Initial Commit

---
+++ Wordle_Project/Wordle_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wordle_ProjectApp.swift
+//  Wordle_Project
+//
+//  Created by student on 12/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct Wordle_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
